91c74f82c254b250ff21663e4bf29cd5ad642be2
JU4N98 <juanpablocabana2@gmail.com>
2021-11-11 10:49:54 -0300

    Corrects postgres error.

--- auditing/db/__init__.py
+++ auditing/db/__init__.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
+import logging
+import time
 
 
-DB_HOST = os.environ["DB_HOST"] 
+DB_HOST = os.environ["DB_HOST"]
 DB_NAME = os.environ["DB_NAME"] 
 DB_USERNAME = os.environ["DB_USERNAME"] 
 DB_PASSWORD = os.environ["DB_PASSWORD"] 
@@ -15,6 +17,14 @@ engine = create_engine('postgresql+psycopg2://{user}:{pw}@{url}:{port}/{db}'.\
 sessionBuilder = sessionmaker()
 sessionBuilder.configure(bind=engine)
 session = sessionBuilder()
+
+while True:
+    try:
+        engine.connect()
+        break
+    except Exception as exc:
+        logging.warning("Couldn't connect with postgres. Retrying connection.")
+        time.sleep(1)
 
 def commit():
     session.commit()
